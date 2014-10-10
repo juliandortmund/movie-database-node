ac27c93a60ff22dab7545bc632c8dd90656391c4
Ben Ripkens <bripkens.dev@gmail.com>
2014-10-10 20:59:23 +0200

    Prepare team information for the next fontys workshop

--- src/repos.js
+++ src/repos.js
@@ -1,22 +1,65 @@
 module.exports = [
   {
-    "name": "fuh-team-1",
+    "name": "2014-10-code-red",
     "collaborators": [
+      "vwochnik",
+      "jenssegschneider",
+      "nilsurban"
     ]
   },
   {
-    "name": "fuh-team-2",
+    "name": "2014-10-code-frantics",
     "collaborators": [
+      "D3vil",
+      "emirzdev",
+      "Jagger911",
+      "Gizmo3399"
     ]
   },
   {
-    "name": "fuh-team-3",
+    "name": "2014-10-localhorsts",
     "collaborators": [
+      "Biohazard90",
+      "dkaisers",
+      "Zwaen91",
+      "woolfred"
     ]
   },
   {
-    "name": "fuh-team-4",
+    "name": "2014-10-Dinatriumdihydrogendiphosphat",
     "collaborators": [
+      "KniKnaKnorke",
+      "MinzigKnusprig",
+      "GER-Jani",
+      "SebekRa"
+    ]
+  },
+  {
+    "name": "2014-10-floppy",
+    "collaborators": [
+      "Jansiko",
+      "Gerry0987654321",
+      "Waldhuette",
+      "gewuerzkahn",
+      "jayremb"
+    ]
+  },
+  {
+    "name": "2014-10-scrumbags",
+    "collaborators": [
+      "undeadpilot",
+      "scrumbag93",
+      "Gwindorith"
+    ]
+  },
+  {
+    "name": "2014-10-typesafe",
+    "collaborators": [
+      "flantony",
+      "Dariyoo",
+      "LKleineBuening",
+      "habilidad",
+      "SimonPNorra"
     ]
   }
-]
+];
